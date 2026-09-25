d59ad6e366d9cf1caabbbf389ceff9233d094f4f
bigcatinthesky <pjbman10@icloud.com>
2026-09-24 20:51:58 -0400

    vtol framework

--- Assets/scripts/Managers/GameObjectBaseType/VehicleBaseType/ShipManager/ShipManager.cs
+++ Assets/scripts/Managers/GameObjectBaseType/VehicleBaseType/ShipManager/ShipManager.cs
@@ -1,22 +1,23 @@
-using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class ShipManager : VehicleTypeBase
 {
     [SerializeField] private ShipProfile shipProfile;
     private ShipMovementSystem shipMovementSystem;
     private ShipControlSystemManager shipControlSystemManager;
+    private ShipVTOLSystem shipVTOLSystem;
     // private ShipLandingSystem shipLandingSystem;
-    // private ShipVTOLSystem shipVTOLSystem;
+
     public ShipProfile ShipProfile { get { return shipProfile; } }
     public ShipMovementSystem ShipMovementSystem { get { return shipMovementSystem; } }
     public ShipControlSystemManager ShipControlSystemManager { get { return shipControlSystemManager; } }
-    public ShipLandingSystem ShipLandingSystem { get { return ShipLandingSystem; } }
+    public ShipVTOLSystem ShipVTOLSystem { get { return shipVTOLSystem;} }
+    // public ShipLandingSystem ShipLandingSystem { get { return ShipLandingSystem; } }
     void Start()
     {
         shipMovementSystem = GetComponent<ShipMovementSystem>();
         shipControlSystemManager = GetComponent<ShipControlSystemManager>();
+        shipVTOLSystem = GetComponent<ShipVTOLSystem>();
     }
 }
--- Assets/scripts/Managers/GameObjectBaseType/VehicleBaseType/ShipManager/ShipVTOLSystem/ShipVTOLSystem.cs
+++ Assets/scripts/Managers/GameObjectBaseType/VehicleBaseType/ShipManager/ShipVTOLSystem/ShipVTOLSystem.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class ShipVTOLSystem : MonoBehaviour
 {
+    private ShipManager shipManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        shipManager = GetComponent<ShipManager>();
     }
 
     // Update is called once per frame
